94a92afccdf15b1828c3a0ba78c2ceaa73419b32
rmuell9 <rmatthewmueller@gmail.com>
2025-07-24 03:57:23 -0500

    harpoon

--- lua/rmmueller/lazy/telescope.lua
+++ lua/rmmueller/lazy/telescope.lua
@@ -7,7 +7,7 @@ return  {
     config = function()
         require('telescope').setup({})
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
--- lua/rmmueller/remap.lua
+++ lua/rmmueller/remap.lua
@@ -10,3 +10,13 @@ vim.keymap.set('n', '<leader>rc', ':RunClose<CR>')
 vim.keymap.set('n', '<leader>lp', ':w<CR>:LivePreview  start<CR>')
 vim.keymap.set('n', '<leader>lc', ':LivePreview close<CR>')
 
+--Harpoon
+vim.keymap.set('n', '<leader>hh', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+vim.keymap.set('n', '<leader>ha', ':lua require("harpoon.mark").add_file()<CR>')
+vim.keymap.set('n', '<leader>hn', ':lua require("harpoon.ui").nav_next()<CR>')
+vim.keymap.set('n', '<leader>hp', ':lua require("harpoon.ui").nav_prev()<CR>')
+vim.keymap.set('n', '<leader>h1', ':lua require("harpoon.ui").nav_file(1)<CR>')
+vim.keymap.set('n', '<leader>h2', ':lua require("harpoon.ui").nav_file(2)<CR>')
+vim.keymap.set('n', '<leader>h3', ':lua require("harpoon.ui").nav_file(3)<CR>')
+vim.keymap.set('n', '<leader>h4', ':lua require("harpoon.ui").nav_file(4)<CR>')
+vim.keymap.set('n', '<leader>h5', ':lua require("harpoon.ui").nav_file(5)<CR>')
