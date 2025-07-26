6bcd0fc968dbaa8e70d491854ff7426288dce3a0
rmuell9 <rmatthewmueller@gmail.com>
2025-07-26 16:20:13 -0500

    plugins

--- lua/rmmueller/remap.lua
+++ lua/rmmueller/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Oil<CR>")
 vim.keymap.set("n", "<leader>m", ":split notepad.md <CR>")
+
+
+vim.keymap.set('n', '%s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/g<Left><Left>')
+
