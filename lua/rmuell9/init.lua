fbd23e6458da03c0f306004acfe036238116f71d
rmuell9 <rmatthewmueller@gmail.com>
2026-01-05 16:11:33 -0600

    update

--- lua/rmuell9/init.lua
+++ lua/rmuell9/init.lua
@@ -5,8 +5,6 @@ require("rmuell9.remap")
 require("rmuell9.set")
 require("rmuell9.lazy_init")
 
-vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
--- lua/rmuell9/lazy/molten.lua
+++ lua/rmuell9/lazy/molten.lua
@@ -1,4 +1,36 @@
 return {
+    {
+        "3rd/image.nvim",
+        build = false,
+        opts = {
+            processor = "magick_cli",
+            backend = "kitty",
+            integrations = {},
+            max_width = 100,
+            max_height = 12,
+            max_height_window_percentage = math.huge,
+            max_width_window_percentage = math.huge,
+            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+            tmux_show_only_in_active_window = true,
+        },
+        keys = {
+            {
+                "<leader>le",
+                function()
+                    require("image").enable()
+                end,
+                desc = "Enable image rendering",
+            },
+            {
+                "<leader>ld",
+                function()
+                    require("image").disable()
+                end,
+                desc = "Disable image rendering",
+            },
+        },
+    },
+    {
         "benlubas/molten-nvim",
         version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
         dependencies = { "3rd/image.nvim" },
@@ -7,7 +39,7 @@ return {
             -- these are examples, not defaults. Please see the readme
             vim.g.molten_image_provider = "image.nvim"
             vim.g.molten_output_win_max_height = 20
-
+            vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
             vim.keymap.set("n", "<leader>jv", ":MoltenEvaluateLine<CR>",
                 { silent = true, desc = "Molten evaluate line" })
             vim.keymap.set("v", "<leader>jv", ":<C-u>MoltenEvaluateVisual<CR>gv",
@@ -17,22 +49,22 @@ return {
             vim.keymap.set("n", "<leader>jd", ":MoltenDelete<CR>",
                 { silent = true, desc = "Molten delete output" })
             vim.keymap.set("n", "<leader>lp", function()
-              local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
-              if venv ~= nil then
-                -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
-                venv = string.match(venv, "/.+/(.+)")
-                vim.cmd(("MoltenInit %s"):format(venv))
-              else
-                -- For uv projects, use current directory name as kernel name
-                local kernel_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-                -- Check if uv project (has pyproject.toml or .venv directory)
-                if vim.fn.filereadable("pyproject.toml") == 1 or 
-                   vim.fn.isdirectory(".venv") == 1 then
-                  vim.cmd(("MoltenInit %s"):format(kernel_name))
+                local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+                if venv ~= nil then
+                    -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+                    venv = string.match(venv, "/.+/(.+)")
+                    vim.cmd(("MoltenInit %s"):format(venv))
                 else
-                  vim.cmd("MoltenInit python3")
+                    -- For uv projects, use current directory name as kernel name
+                    local kernel_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+                    -- Check if uv project (has pyproject.toml or .venv directory)
+                    if vim.fn.filereadable("pyproject.toml") == 1 or 
+                        vim.fn.isdirectory(".venv") == 1 then
+                        vim.cmd(("MoltenInit %s"):format(kernel_name))
+                    else
+                        vim.cmd("MoltenInit python3")
+                    end
                 end
-              end
             end, { desc = "Initialize Molten for python3", silent = true })
 
             -- I find auto open annoying, keep in mind setting this option will require setting
@@ -53,4 +85,5 @@ return {
             -- this will make it so the output shows up below the \`\`\` cell delimiter
             vim.g.molten_virt_lines_off_by_1 = true
         end,
+    }
 }
