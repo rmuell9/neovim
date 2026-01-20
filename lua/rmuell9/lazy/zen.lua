b7bd1624ab9a58749ff40c80c31b157136ddc139
rmuell9 <rmatthewmueller@gmail.com>
2026-01-19 19:44:09 -0600

    update

--- lua/rmuell9/lazy/zen.lua
+++ lua/rmuell9/lazy/zen.lua
@@ -1,7 +1,28 @@
 return {
     "folke/zen-mode.nvim",
     config = function()
+        local function is_normal_buffer()
+            local buftype = vim.bo.buftype
+            local filetype = vim.bo.filetype
+            local excluded_filetypes = { "oil", "neo-tree", "NvimTree", "TelescopePrompt" }
+            
+            if buftype ~= "" then
+                return false
+            end
+            
+            for _, ft in ipairs(excluded_filetypes) do
+                if filetype == ft then
+                    return false
+                end
+            end
+            
+            return true
+        end
+
         vim.keymap.set("n", "<leader>zz", function()
+            if not is_normal_buffer() then
+                return
+            end
             require("zen-mode").setup {
                 window = {
                     width = 90,
@@ -16,6 +37,9 @@ return {
 
 
         vim.keymap.set("n", "<leader>zZ", function()
+            if not is_normal_buffer() then
+                return
+            end
             require("zen-mode").setup {
                 window = {
                     width = 80,
@@ -51,6 +75,9 @@ return {
             pattern = "markdown",
             callback = function()
                 vim.schedule(function()
+                    if not is_normal_buffer() then
+                        return
+                    end
                     vim.api.nvim_feedkeys(
                         vim.api.nvim_replace_termcodes(
                             "<leader>zZ", true, false, true
