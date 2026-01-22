274c31b42101a59c337e51fca129a60d4be72073
rmuell9 <rmatthewmueller@gmail.com>
2026-01-21 18:17:46 -0600

    update

---
+++ lua/rmuell9/lazy/themes.lua
@@ -0,0 +1,13 @@
+return {
+    "xiyaowong/transparent.nvim",
+
+    -- Preload colorschemes
+    "navarasu/onedark.nvim",
+    "ellisonleao/gruvbox.nvim",
+	"bjarneo/hackerman.nvim",
+	"bjarneo/aether.nvim",
+	"rose-pine/neovim",
+    "folke/tokyonight.nvim",
+    "shaunsingh/nord.nvim",
+    "kepano/flexoki-neovim",
+}
